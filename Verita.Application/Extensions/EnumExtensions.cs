7b63d97ca8b73ac182c3f146eb1ae4965f06fe82
Ali Fuat Yıldırım <ali.yildirim@turk.net>
2025-01-27 22:25:16 +0300

    son güncellemeler

---
+++ Verita.Application/Extensions/EnumExtensions.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+
+namespace Verita.Application.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum enumValue, params object?[] args)
+        {
+            string enumText = enumValue.ToString();
+
+            FieldInfo fi = enumValue.GetType().GetField(enumText)!;
+
+            string? description = fi.GetCustomAttributes(typeof(DescriptionAttribute), false)
+                         .Cast<DescriptionAttribute>()
+                         .Select(x => x.Description)
+                         .FirstOrDefault();
+
+            if (string.IsNullOrEmpty(description))
+            {
+                return enumText;
+            }
+
+            try
+            {
+                return args.Length > 0 ? string.Format(CultureInfo.InvariantCulture, description, args) : description;
+            }
+            catch (FormatException)
+            {
+                return enumText;
+            }
+            catch (ArgumentNullException)
+            {
+                return enumText;
+            }
+        }
+
+        public static string GetDescription(this Type enumType, string enumText, params object?[] args)
+        {
+            FieldInfo fi = enumType.GetField(enumText)!;
+
+            string? description = fi.GetCustomAttributes(typeof(DescriptionAttribute), false)
+                .Cast<DescriptionAttribute>()
+                .Select(x => x.Description)
+                .FirstOrDefault();
+
+            if (string.IsNullOrEmpty(description))
+            {
+                return enumText;
+            }
+
+            try
+            {
+                return args.Length > 0 ? string.Format(CultureInfo.InvariantCulture, description, args) : description;
+            }
+            catch (FormatException)
+            {
+                return enumText;
+            }
+            catch (ArgumentNullException)
+            {
+                return enumText;
+            }
+        }
+
+        public static string? GetDescriptionByString<T>(this string? value)
+            where T : struct, Enum
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return null;
+            }
+
+            if (!Enum.TryParse<T>(value, out T result) || !Enum.IsDefined(typeof(T), value))
+            {
+                return null;
+            }
+
+            return result.GetDescription();
+        }
+    }
+}
--- Verita.Common/Enums/PageCategory.cs
+++ Verita.Common/Enums/PageCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,13 @@ namespace Verita.Common.Enums
 {
     public enum PageCategory
     {
+        [Description("Güvenli Gıda")]
         GuvenliGida=0,
+        [Description("Merak Ettikleriniz")]
         MerakEttikleriniz=1,
+        [Description("B2b Ürünler")]
         B2b=2,
+        [Description("Ürünlerimiz")]
         Urunlerimiz=3, 
     }
 }
